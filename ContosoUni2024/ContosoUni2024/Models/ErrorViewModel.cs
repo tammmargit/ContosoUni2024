0124f20475f09066829fa5ba08cc46282a794b10
Margit Tammeorg <>
2024-08-26 11:36:12 +0300

    Init

---
+++ ContosoUni2024/ContosoUni2024/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ContosoUni2024.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
